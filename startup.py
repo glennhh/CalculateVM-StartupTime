13ae9a2142c7f05e3f91d5f1d2323015c9221050
Radhika Bhavsar <radhikabhavsar@Radhikas-MacBook-Pro.local>
2019-09-07 02:16:56 -0400

    Updated VM creation script to specify VM image size

--- startup.py
+++ startup.py
@@ -1,6 +1,15 @@
 import boto3
 client = boto3.client('ec2')
 response = client.run_instances( ImageId='ami-00c03f7f7f2ec15c3',
+                                 BlockDeviceMappings=[
+                                     { 'DeviceName': '/dev/xvda',
+                                       'Ebs': {
+                                           'DeleteOnTermination': True,
+                                           'VolumeSize': 16,
+                                           'VolumeType': 'gp2'
+                                       },
+                                     },
+                                 ],
                                  InstanceType='t2.micro',
                                  MinCount=1,
                                  MaxCount=1 )
